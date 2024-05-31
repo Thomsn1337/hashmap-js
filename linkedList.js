f96c2940cfe6e1dc0481da4725f082b26015d7c0
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-31 18:19:39 +0200

    implement basic linked list

---
+++ linkedList.js
@@ -0,0 +1,68 @@
+class ListNode {
+    /**
+     * @constructor
+     * @param {string} key
+     * @param {any} value
+     */
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    /**
+     * @type {ListNode | null}
+     */
+    #head;
+
+    /**
+     * @type {ListNode | null}
+     */
+    #tail;
+
+    constructor() {
+        this.#head = null;
+        this.#tail = null;
+    }
+
+    /**
+     * @param {string} key
+     * @param {any} value
+     */
+    append(key, value) {
+        const node = new ListNode(key, value);
+
+        if (this.#head === null) {
+            this.#head = node;
+            this.#tail = node;
+            return;
+        }
+
+        if (this.#tail !== null) {
+            const current = this.#tail;
+            current.next = node;
+            this.#tail = node;
+        }
+    }
+
+    toString() {
+        let current = this.#head;
+        let listString = "";
+
+        if (!current) listString = "()";
+
+        while (current) {
+            if (current.next)
+                listString += `([${current.key}, ${current.value}]) -> `;
+            else listString += `([${current.key}, ${current.value}])`;
+
+            current = current.next;
+        }
+
+        return listString;
+    }
+}
+
+export default LinkedList;
