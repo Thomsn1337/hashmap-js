9c29140040cf992b08f18614fa1613ee14ff8a6f
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-31 19:14:39 +0200

    add method to add new items to hash map

--- hashMap.js
+++ hashMap.js
@@ -10,17 +10,20 @@ class HashMap {
      */
     constructor(capacity = 16, loadFactor = 0.75) {
         this.#capacity = capacity;
+        this.loadFactor = loadFactor;
+
+        /**
+         * @type {HashMap[]}
+         */
         this.#buckets = new Array(this.#capacity)
             .fill(null)
             .map(() => new LinkedList());
-
-        this.loadFactor = loadFactor;
     }
 
     /**
      * @param {string} key
      */
-    hash(key) {
+    #hash(key) {
         let code = 0;
         const primeNumber = 13;
 
@@ -29,6 +32,22 @@ class HashMap {
         }
 
         return code;
+    }
+
+    /**
+     * @param {string} key
+     * @param {any} value
+     */
+    set(key, value) {
+        const hashCode = this.#hash(key);
+        const bucket = this.#buckets[hashCode];
+        const node = bucket.getNode(key);
+
+        if (!node) {
+            bucket.append(key, value);
+        } else {
+            node.value = value;
+        }
     }
 }
 
